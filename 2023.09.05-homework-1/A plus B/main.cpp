813ee329d74245096c2d44b429c5c4805f301fef
st116552 <144111896+st116552@users.noreply.github.com>
2023-09-05 22:34:57 +0300

    answer1

--- 2023.09.05-homework-1/A plus B/main.cpp
+++ 2023.09.05-homework-1/A plus B/main.cpp
@@ -1,7 +1,16 @@
 #include<iostream>
+#include <fstream>
 
 using namespace std;
 
 int main() {
-
+    int a,b;
+    ifstream fin;
+    fin.open("input.txt");
+    fin >> a >> b;
+    fin.close();
+    ofstream fout;
+    fout.open("output.txt");
+    fout << a + b;
+    fout.close();
 }
