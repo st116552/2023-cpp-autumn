fbb76aad2f0d33d5f163581d08a2b5b4a7a15272
st116552 <144111896+st116552@users.noreply.github.com>
2023-09-06 11:34:36 +0300

    v1

---
+++ 2023.09.05-homework-1/Max/main.cpp
@@ -0,0 +1,17 @@
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    ifstream fin;
+    fin.open("input.txt");
+    fin >> a >> b;
+    fin.close();
+    ofstream fout;
+    fout.open("output.txt");
+    fout << (a / b * a + b / a * b) / (a / b + b / a);
+    fout.close();
+    return 0;
+}
