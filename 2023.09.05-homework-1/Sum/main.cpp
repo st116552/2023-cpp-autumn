08062d926bccb70ad17bfb740d1be5467e6fcee3
st116552 <144111896+st116552@users.noreply.github.com>
2023-09-08 18:38:02 +0300

    v3 delete if

--- 2023.09.05-homework-1/Sum/main.cpp
+++ 2023.09.05-homework-1/Sum/main.cpp
@@ -11,12 +11,7 @@ int main()
     fin.open("input.txt");
     fin >> n;
     fin.close();
-    if (n <= 0) {
-        sum = (n + 1) * (2 - n) / 2;
-    } else {
-        sum = (n + 1) * n / 2;
-    }
-    //sum = ((n + 1) * (abs(n) + 1 - n / abs(n))) / 2;
+    sum = (n + 1) * (abs(n - 1) + 1) / 2;
     ofstream fout;
     fout.open("output.txt");
     fout << sum;
