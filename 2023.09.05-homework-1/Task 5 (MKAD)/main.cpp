5b1fc977cfeb2338044c6681a44e1b642ca42f8b
st116552 <144111896+st116552@users.noreply.github.com>
2023-09-09 17:44:45 +0300

    add initialization on declaration

--- 2023.09.05-homework-1/Task 5 (MKAD)/main.cpp
+++ 2023.09.05-homework-1/Task 5 (MKAD)/main.cpp
@@ -4,13 +4,15 @@ using namespace std;
 
 int main() 
 {
-    long long v, t;
-    long long road = 109 * 1000000 + 1;
+    int const roadLength = 109;
+    long long v = 0;
+    long long t = 0;
+    long long road = roadLength * 1000000 + 1;
     ifstream fin;
     fin.open("input.txt");
     fin >> v >> t;
     fin.close();
-    road = (road + v * t - 1) % 109 + 1;
+    road = (road + v * t - 1) % roadLength + 1;
     ofstream fout;
     fout.open("output.txt");
     fout << road;
