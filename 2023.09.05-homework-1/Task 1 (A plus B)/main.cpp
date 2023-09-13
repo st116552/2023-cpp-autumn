4b23b6d025946e889efb8170be9b71ce31f418ca
st116552 <144111896+st116552@users.noreply.github.com>
2023-09-13 10:55:33 +0300

    change fstream to iostream

--- 2023.09.05-homework-1/Task 1 (A plus B)/main.cpp
+++ 2023.09.05-homework-1/Task 1 (A plus B)/main.cpp
@@ -1,18 +1,10 @@
-#include <fstream>
-
-using namespace std;
+#include <iostream>
 
 int main() 
 {
     int a = 0;
     int b = 0;
-    ifstream fin;
-    fin.open("input.txt");
-    fin >> a >> b;
-    fin.close();
-    ofstream fout;
-    fout.open("output.txt");
-    fout << a + b;
-    fout.close();
+    std::cin >> a >> b;
+    std::cout << a + b;
     return 0;
 }
