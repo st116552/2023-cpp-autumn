c25dcb1eacc98db4106fa3d6a4ecd58be8c65abd
st116552 <144111896+st116552@users.noreply.github.com>
2023-09-13 12:56:47 +0300

    no abs (no #include <math.h>)

--- 2023.09.05-homework-1/Task 2 (Sum)/main.cpp
+++ 2023.09.05-homework-1/Task 2 (Sum)/main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-#include <stdlib.h>
 
 int main() 
 {
     int n = 0;
     int sum = 0;
     std::cin >> n;
-    sum = (n + 1) * (abs(n - 1) + 1) / 2;
+    sum = (n >= 1) * n * (n + 1) / 2 + (n < 1) * (2 - n) * (n + 1) / 2;
     std::cout << sum;
     return 0;
 }
