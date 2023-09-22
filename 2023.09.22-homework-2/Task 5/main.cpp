a0a4854d2f05c793d9b931f34295eb3e9a9526b9
st116552 <144111896+st116552@users.noreply.github.com>
2023-09-23 02:04:36 +0300

    v1

---
+++ 2023.09.22-homework-2/Task 5/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() 
+{
+    int n = 0;
+    std::cin >> n;
+    if (n % 10 == 1 && n / 10 % 10 != 1) 
+    {
+        std::cout << n << " bochka";
+    }
+    else if ((n % 10 == 2 || n % 10 == 3 || n % 10 == 4) && n / 10 % 10 != 1)
+    {
+        std::cout << n << " bochki";
+    }
+    else
+    {
+        std::cout << n << " bochek";
+    }
+
+}
