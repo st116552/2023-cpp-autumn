95ac7429e9a9aa8c0e8290a863cb926677ca608d
st116552 <144111896+st116552@users.noreply.github.com>
2024-04-04 17:39:05 +0300

    2024.04.04 17:38

--- 2024.03.29-homework-2.1/task5/main.cpp
+++ 2024.03.29-homework-2.1/task5/main.cpp
@@ -2,13 +2,9 @@
  
 int main() {
     int n = 0;
-    int a1 = 0;
-    int a2 = 0;
+    long long a1 = 0;
+    long long a2 = 0;
     std::cin >> n >> a1 >> a2;
-    // for (int i = 0; i < n && a1 != a2; i++) {
-    //     a1 = a1 / 2;
-    //     a2 = a2 / 2;
-    // }
     while (a1 != a2) {
         if (a1 > a2) {
             a1 = a1 / 2;
@@ -16,6 +12,6 @@ int main() {
             a2 = a2 / 2;
         }
     }
-    std::cout << a2;
+    std::cout << a1;
     return 0;
 }
